4ed5f7a0448150dceadea40ee6d3602c616faa31
Anoir Jankowski <jankowskianoir0@gmail.com>
2026-03-30 13:41:58 +0200

    add button input functionality to calculator

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Calculator started");
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    const value = button.getAttribute("data-value");
+    display.value += value;
+  });
+});
