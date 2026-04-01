cc4767ec2a846d1ba49aff5445cec4d39ad7c439
Anoir Jankowski <jankowskianoir0@gmail.com>
2026-04-01 11:10:49 +0200

    implement basic calculator result logic

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach(function(button) {
   button.addEventListener("click", function() {
     const value = button.getAttribute("data-value");
-    display.value += value;
+
+    if(value === "=") {
+      try {
+        //eval calculates the math expression in the display
+        display.value = eval(display.value);
+      } catch (error) {
+        display.value= "Error";
+      }
+    } else {
+      display.value += value;
+    }
   });
 });
