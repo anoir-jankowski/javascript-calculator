2941d744440f1720135554d321d8138ba9782f92
Anoir Jankowski <jankowskianoir0@gmail.com>
2026-04-07 10:14:17 +0200

    implement clear functionality

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll("button");
 
+// loop through all buttons
 buttons.forEach(function(button) {
   button.addEventListener("click", function() {
+
+    // get value from button
     const value = button.getAttribute("data-value");
 
     if(value === "=") {
       try {
-        //eval calculates the math expression in the display
+        // eval calculates the math expression in the display
         display.value = eval(display.value);
       } catch (error) {
         display.value= "Error";
       }
+    } else if (value=== "C") {
+      display.value ="";
+      
     } else {
       display.value += value;
     }
