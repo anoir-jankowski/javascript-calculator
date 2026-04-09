956e69eaf416cf90f0c1b4e9087236bdd8a9078c
Anoir Jankowski <jankowskianoir0@gmail.com>
2026-04-09 10:23:16 +0200

    improve calculator input validation (prevent invalid operators)

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll("button");
 
+// list of allowed operators
+const operatots = ["+", "-", "*", "/"]
+
 // loop through all buttons
 buttons.forEach(function(button) {
   button.addEventListener("click", function() {
 
     // get value from button
     const value = button.getAttribute("data-value");
+    if(value ==="C") {
+      display.value = "";
+      return;
+    }
 
     if(value === "=") {
       try {
@@ -15,11 +22,20 @@ buttons.forEach(function(button) {
       } catch (error) {
         display.value= "Error";
       }
-    } else if (value=== "C") {
-      display.value ="";
-      
-    } else {
-      display.value += value;
+      return;
     }
+      // gets last character (important for checks)
+      const lastChar = display.value.slice(-1);
+
+      // prevent starting with an operator
+      if(display.value === "" && operators.includes(value)) {
+        return;
+    }  
+
+      // prevent double operators
+      if (operatots.includes(value) && operators.includes(lastChar)){
+        return;
+      }
+      display.value += value;
   });
 });
